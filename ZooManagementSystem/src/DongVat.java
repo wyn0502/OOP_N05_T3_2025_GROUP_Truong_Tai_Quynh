cfbe9677802af834307c4b96b71a219d11a6b6f0
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-06-18 08:24:41 +0000

    DongVat.java step1

---
+++ ZooManagementSystem/src/DongVat.java
@@ -0,0 +1,10 @@
+package ZooManagementSystem.src;
+
+public class DongVat {
+    String maDongVat;
+    String tenDongVat;
+    String Loai;
+    int Tuoi;
+    String ChuongSo;
+    String ThoiGianChoAn; 
+}
