9703be452f48a2d1ef8c39eb1b61a5de1d909780
Đặng Tài <dangductai0510@gmail.com>
2025-06-25 08:42:43 +0000

    update lich cho an

--- ZooManagementSystem/src/App.java
+++ ZooManagementSystem/src/App.java
@@ -1,11 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
-        TestGiaVe.Inve();
-        PassObject.main(new MyNumber());
-        BreakAndContinue.testFor();
-        BreakAndContinue.testWhile();
-        BreakAndContinue.testDoWhile();
+        // TestGiaVe.Inve();
+        // PassObject.main(new MyNumber());
+        // BreakAndContinue.testFor();
+        // BreakAndContinue.testWhile();
+        // BreakAndContinue.testDoWhile();
+        TestLichChoAn.inthoigian();
     }
 }
