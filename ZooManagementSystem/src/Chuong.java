fefd01ccc5f71d4b6499d8946ea8853542838177
xuantruong1612 <alonesera1612@gmail.com>
2025-06-25 15:37:14 +0700

    cau 4 5

--- ZooManagementSystem/src/Chuong.java
+++ ZooManagementSystem/src/Chuong.java
@@ -1,8 +1,18 @@
-package ZooManagementSystem.src;
-
 public class Chuong {
     String MaChuong;
     String Tenkhuvuc;
     int SucChuaToiDa;
     int SoLuongHienTai;
+
+    public Chuong(String maChuong, String Tenkhuvuc, int SucChuaToiDa) {
+        this.MaChuong = MaChuong;
+        this.Tenkhuvuc = Tenkhuvuc;
+        this.SucChuaToiDa = SucChuaToiDa;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Ma chuong: " + MaChuong);
+        System.out.println("Ten khu vuc: " + Tenkhuvuc);
+        System.out.println("Suc chua: " + SucChuaToiDa);
+    }
 }
