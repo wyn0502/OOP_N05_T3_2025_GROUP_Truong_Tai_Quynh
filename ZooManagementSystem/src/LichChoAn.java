9effdd2c48f20b07248ba07e6dbee542be1c7f0f
truong <alonesera1612@gmail.com>
2025-06-25 18:01:57 +0700

    format code

--- ZooManagementSystem/src/LichChoAn.java
+++ ZooManagementSystem/src/LichChoAn.java
@@ -3,58 +3,34 @@
 package ZooManagementSystem.src;
 
 public class LichChoAn {
-    
+
     private String maLich;
     private String dongVat;
     private String thucAn;
     private String nhanVienChoAn;
     private String thoiGian;
 
-    
-    public LichChoAn() {
-    }
+    public LichChoAn() {}
 
-    
-    public void vietMaLich(String maLich) {
-        this.maLich = maLich;
-    }
+    public void vietMaLich(String maLich) { this.maLich = maLich; }
 
-    public String hienThiMaLich() {
-        return maLich;
-    }
+    public String hienThiMaLich() { return maLich; }
 
-    public void chonDongVat(String dongVat) {
-        this.dongVat = dongVat;
-    }
+    public void chonDongVat(String dongVat) { this.dongVat = dongVat; }
 
-    public String hienThiDongVat() {
-        return dongVat;
-    }
+    public String hienThiDongVat() { return dongVat; }
 
-    public void chonThucAn(String thucAn) {
-        this.thucAn = thucAn;
-    }
+    public void chonThucAn(String thucAn) { this.thucAn = thucAn; }
 
-    public String hienThiThucAn() {
-        return thucAn;
-    }
+    public String hienThiThucAn() { return thucAn; }
 
     public void chonNhanVienChoAn(String nhanVienChoAn) {
         this.nhanVienChoAn = nhanVienChoAn;
     }
 
-    public String hienThiNhanVienChoAn() {
-        return nhanVienChoAn;
-    }
+    public String hienThiNhanVienChoAn() { return nhanVienChoAn; }
 
-    public void chonThoiGian(String thoiGian) {
-        this.thoiGian = thoiGian;
-    }
+    public void chonThoiGian(String thoiGian) { this.thoiGian = thoiGian; }
 
-    public String hienThiThoiGian() {
-        return thoiGian;
-    }
-
-    
-    
+    public String hienThiThoiGian() { return thoiGian; }
 }
