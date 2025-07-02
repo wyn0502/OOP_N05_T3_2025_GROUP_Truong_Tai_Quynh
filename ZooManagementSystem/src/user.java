7f48d560450792de331a96b8c92c18cb10af8c15
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-07-02 06:27:23 +0000

    khai bao user 1

--- ZooManagementSystem/src/user.java
+++ ZooManagementSystem/src/user.java
@@ -1,3 +1,12 @@
 public class user {
-    
+    private String HoTen;
+    private String email;
+
+    public user() {
+    }
+
+    public user(String hoTen, String email) {
+        this.HoTen = hoTen;
+        this.email = email;
+    }
 }
