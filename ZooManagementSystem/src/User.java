101d223fd4abc936843575527f83d2889acbcfae
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-07-02 07:36:52 +0000

    add User.Java

--- ZooManagementSystem/src/User.java
+++ ZooManagementSystem/src/User.java
@@ -1,37 +1,104 @@
 public class User {
-    private String HoTen;
+    private String hoTen;
     private String email;
+    private String diaChi;
+    private String sdt;
+    private String ngaySinh;
+    private int tuoi;
+    private String maSV;
+
 
     public User() {
     }
 
-    public User(String HoTen, String email) {
-        this.HoTen = HoTen;
+
+    public User(String hoTen, String email, String diaChi, String sdt, String ngaySinh, int tuoi, String maSV) {
+        this.hoTen = hoTen;
         this.email = email;
+        this.diaChi = diaChi;
+        this.sdt = sdt;
+        this.ngaySinh = ngaySinh;
+        this.tuoi = tuoi;
+        this.maSV = maSV;
     }
 
     public String getHoTen() {
-        return HoTen;
+        return hoTen;
     }
 
-    public void setHoTen(String HoTen) {
-        this.HoTen = HoTen;
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail() {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String toString() {
-        return "Họ tên: " + HoTen + ", Email: " + email;
+    public String getDiaChi() {
+        return diaChi;
     }
 
-    public static void main(String[] args){
-        User u = new User("Lò Tuấn Quỳnh", "24104502@st.phenikaa-uni.edu.vn");
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+
+    public String toString() {
+        return "Họ tên: " + hoTen +
+               "\nEmail: " + email +
+               "\nĐịa chỉ: " + diaChi +
+               "\nSĐT: " + sdt +
+               "\nNgày sinh: " + ngaySinh +
+               "\nTuổi: " + tuoi +
+               "\nMã sinh viên: " + maSV;
+    }
+
+    public static void main(String[] args) {
+        User u = new User(
+            "Lò Tuấn Quỳnh",
+            "24104502@st.phenikaa-uni.edu.vn",
+            "Điện Biên",
+            "0342135006",
+            "05/02/2006",
+            19,
+            "24104502"
+        );
         System.out.println(u);
-}
+    }
 }
