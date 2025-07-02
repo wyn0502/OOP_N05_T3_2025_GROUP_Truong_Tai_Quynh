3e7ec49da2b2e95cb7ffa11883ed3d8caee037bb
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-07-02 07:08:42 +0000

    fix Time.java

--- ZooManagementSystem/src/Time.java
+++ ZooManagementSystem/src/Time.java
@@ -1,34 +1,62 @@
-public void setTime(int gio, int phut, int giay) {
-    setGio(gio);
-    setPhut(phut);
-    setGiay(giay);
-}
+import java.sql.Time;
+public class Time {
+    private int gio;
+    private int phut;
+    private int giay;
 
-public void setGio(int gio) {
-    this.gio = (gio >= 0 && gio < 24) ? gio : 0;
-}
+    public Time() {
+        setTime(0, 0, 0);
+    }
 
-public void setPhut(int phut) {
-    this.phut = (phut >= 0 && phut < 60) ? phut : 0;
-}
+    public Time(int gio) {
+        setTime(gio, 0, 0);
+    }
 
-public void setGiay(int giay) {
-    this.giay = (giay >= 0 && giay < 60) ? giay : 0;
-}
+    public Time(int gio, int phut) {
+        setTime(gio, phut, 0);
+    }
 
-public int getGio() {
-    return gio;
-}
+    public Time(int gio, int phut, int giay) {
+        setTime(gio, phut, giay);
+    }
 
-public int getPhut() {
-    return phut;
-}
+    public void setTime(int gio, int phut, int giay) {
+        setGio(gio);
+        setPhut(phut);
+        setGiay(giay);
+    }
 
-public int getGiay() {
-    return giay;
-}
+    public void setGio(int gio) {
+        this.gio = (gio >= 0 && gio < 24) ? gio : 0;
+    }
 
-public String toString() {
-    return String.format("%02d:%02d:%02d", gio, phut, giay);
+    public void setPhut(int phut) {
+        this.phut = (phut >= 0 && phut < 60) ? phut : 0;
+    }
+
+    public void setGiay(int giay) {
+        this.giay = (giay >= 0 && giay < 60) ? giay : 0;
+    }
+
+    public int getGio() {
+        return gio;
+    }
+
+    public int getPhut() {
+        return phut;
+    }
+
+    public int getGiay() {
+        return giay;
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d", gio, phut, giay);
+    }
+
+public static void main(String[] args){
+    Time t = new Time(9, 30, 45);
+        System.out.println("Thời gian: " + t);
+
 }
 }
