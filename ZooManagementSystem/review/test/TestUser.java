6d7cc2f61a647a293939461deb3b7d17b73828e8
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-07-02 07:53:33 +0000

    test - chua done

--- ZooManagementSystem/review/test/TestUser.java
+++ ZooManagementSystem/review/test/TestUser.java
@@ -1,5 +1,16 @@
-package test;
-
 public class TestUser {
-    
+    public static void main(String[] args) {
+        User u = new User(
+            "Lò Tuấn Quỳnh",
+            "24104502@st.phenikaa-uni.edu.vn",
+            "Điện Biên",
+            "0342135006",
+            "05/02/2006",
+            19,
+            "24104502"
+        );
+
+        System.out.println("=== Thông tin User ===");
+        System.out.println(u);
+    }
 }
--- ZooManagementSystem/src/Recursion.java
+++ ZooManagementSystem/src/Recursion.java
@@ -1,12 +1,6 @@
 public class Recursion {
     public long tinhGiaiThua(int n) {
-        if (n <= 1)
-            return 1;
-        else
-            return n * tinhGiaiThua(n - 1);
+        if (n <= 1) return 1;
+        return n * tinhGiaiThua(n - 1);
     }
-    public static void main(String[] args){
-        Recursion r = new Recursion();
-        System.out.println("Giai thừa 5 là: " + r.tinhGiaiThua(5));;
-}
 }
