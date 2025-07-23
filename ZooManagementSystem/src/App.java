53f7882e368816431dd08f093e3d8f8cb31c39cf
xuantruong1612 <alonesera1612@gmail.com>
2025-07-23 13:18:26 +0700

    sua

--- ZooManagementSystem/src/App.java
+++ ZooManagementSystem/src/App.java
@@ -1,23 +1,16 @@
 
-<<<<<<< HEAD
-
-// import test.TestChuongManager;
 import test.TestLichChoAnManager;
-=======
-<<<<<<< HEAD
-import src.TestLichChoAn;
-=======
-import test.TestChuongManager;
->>>>>>> 9032f0afad23947dd899ed813f41c88c784dc992
 
->>>>>>> ecb26966c16f3788113155a9565a7972fe134be2
+import src.TestLichChoAn;
+import test.TestChuongManager;
+
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class App {
-<<<<<<< HEAD
-//    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
 //        List<LichChoAn> ds = new ArrayList<>();
 //        Scanner scan = new Scanner(System.in);
 //        LichChoAnManager manager = testLichChoAnManager.getManager();
@@ -52,49 +45,8 @@ public class App {
 
         // TestGiaVeManager test = new TestGiaVeManager();
         // test.chạyThu();
-        TestLichChoAn.runTest(); 
-=======
-    public static void main(String[] args) throws Exception {
-        //        List<LichChoAn> ds = new ArrayList<>();
-        //        Scanner scan = new Scanner(System.in);
-        //        LichChoAnManager manager = testLichChoAnManager.getManager();
-        //
-        //        System.out.println("Danh sách ban đầu");
-        //        manager.printLichChoAn();
-        //
-        //        System.out.println("\nTạo thêm lịch mới");
-        //        LichChoAn newLich = new LichChoAn("004", "Gấu", "Mật ong",
-        //        "NV04", "11:00"); manager.Create(newLich);
-        //        manager.printLichChoAn();
-        //
-        //        System.out.println("\nXóa lịch có mã '002'");
-        //        manager.Delete("002");
-        //        manager.printLichChoAn();
-        //
-        //        System.out.println("\nSửa thời gian có mã '003'");
-        //        manager.Edit("003");
-        //        manager.printLichChoAn();
-        //
-        //
-        //
-        //
-        // testChuongManager test = new testChuongManager();
-        // test.chayThu();
-        //
-        // TestDongVatManager test = new TestDongVatManager();
-        // test.chayThu();
-        //
-        // TestTH5_CellPhone test2 = new TestTH5_CellPhone();
-        // test2.testTune();
-
-        // TestGiaVeManager test3 = new TestGiaVeManager();
-        // test3.chayThu(new String[] {});
-
-        // TestChuongManager.runTest();
-        TestLichChoAnManager.runTest();
+        TestLichChoAn.runTest();
     }
->>>>>>> ecb26966c16f3788113155a9565a7972fe134be2
-
 }
     
     
