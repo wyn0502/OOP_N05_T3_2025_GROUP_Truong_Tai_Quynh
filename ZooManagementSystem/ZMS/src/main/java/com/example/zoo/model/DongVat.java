c8bc8c516e5e4b681fb09a6785d86b4651fe10cb
Xuân Trường <alonesera1612@gmail.com>
2025-08-01 13:11:00 +0000

    UI

--- ZooManagementSystem/ZMS/src/main/java/com/example/zoo/model/DongVat.java
+++ ZooManagementSystem/ZMS/src/main/java/com/example/zoo/model/DongVat.java
@@ -1,21 +1,47 @@
 package com.example.zoo.model;
 
+import java.util.Random;
+
 /**
  * @author TRUONG
  */
-
 public class DongVat {
+    private String id;
     private String ten;
     private int tuoi;
     private String loai;
 
     public DongVat() {
+        this.id = generateRandomId();
     }
 
     public DongVat(String ten, int tuoi, String loai) {
+        this.id = generateRandomId();
         this.ten = ten;
         this.tuoi = tuoi;
         this.loai = loai;
+    }
+
+    // Tạo ID ngẫu nhiên 6 ký tự (chữ và số)
+    private String generateRandomId() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder result = new StringBuilder();
+        Random random = new Random();
+        
+        for (int i = 0; i < 6; i++) {
+            result.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        
+        return result.toString();
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTen() {
@@ -43,6 +69,7 @@ public class DongVat {
     }
 
     public void hienThiThongTin() {
+        System.out.println("ID: " + id);
         System.out.println("Ten: " + ten);
         System.out.println("Tuoi: " + tuoi);
         System.out.println("Loai: " + loai);
@@ -56,8 +83,27 @@ public class DongVat {
             case "su tu":
                 System.out.println("Su tu: Gam gu!");
                 break;
+            case "voi":
+                System.out.println("Voi: Pawoo!");
+                break;
+            case "cop":
+                System.out.println("Cop: Roar roar!");
+                break;
+            case "gau":
+                System.out.println("Gau: Groooowl!");
+                break;
             default:
                 System.out.println("Dong vat: ...");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "DongVat{" +
+                "id='" + id + '\'' +
+                ", ten='" + ten + '\'' +
+                ", tuoi=" + tuoi +
+                ", loai='" + loai + '\'' +
+                '}';
     }
 }
