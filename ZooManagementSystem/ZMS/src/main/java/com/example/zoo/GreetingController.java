fe7817a2ec8e74d4e41e9cff0e75cca6a2c31d51
Wyn <24104502@st.phenikaa-uni.edu.vn>
2025-08-05 10:41:02 +0000

    UP

--- ZooManagementSystem/ZMS/src/main/java/com/example/zoo/GreetingController.java
+++ ZooManagementSystem/ZMS/src/main/java/com/example/zoo/GreetingController.java
@@ -2,6 +2,8 @@ package com.example.zoo;
 
 import com.example.zoo.model.DongVat;
 import com.example.zoo.model.GiaVe;
+import com.example.zoo.model.NhanVien;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,26 +28,27 @@ public class GreetingController {
             Model model) {
 
         DongVat dv = new DongVat("Gà Tre daden", 2, "Gà");
-        
+
         GiaVe gv = new GiaVe("ve1612", "Vé Trẻ Em", 100000.0, "Vui hè", 50.0);
+
+        List<NhanVien> danhSachNhanVien = new ArrayList<>();
+        danhSachNhanVien.add(new NhanVien("NV01", "Lò Tuấn Quỳnh", "Quản lý", "10 năm", "0342135006", "Capybara"));
 
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh"));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-        
         model.addAttribute("name", name);
         model.addAttribute("dongVat", dv);
         model.addAttribute("giaVe", gv);
         model.addAttribute("danhSachNhanVien", danhSachNhanVien);
         model.addAttribute("currentTime", now.format(formatter));
         model.addAttribute("timezone", "GMT+7 (Việt Nam)");
-        
+
         return "greeting";
     }
 
-    // Trang gốc sẽ chuyển về trang quản lý lịch cho ăn
     @GetMapping("/")
     public String home() {
-        return "redirect:/lichchoan";
+        return "redirect:/greeting";
     }
 }
